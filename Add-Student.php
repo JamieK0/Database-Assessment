acc13a2861b45377a56968b72752fa540d1e2571
j14273 <jamie.kyriacou101@gmail.com>
2024-06-13 09:20:31 +1000

    Working add student, submit button, page 1

--- Add-Student.php
+++ Add-Student.php
@@ -59,14 +59,14 @@
     $StuNumber = test_input($_POST["StuNumber"]);
 
     // Inserts into database
-      $query = "INSERT INTO `Student_Info` (`First_Name`, `Last_Name`, `Grade`, `PC_Class`, `Student_Number`, `DOB`, `House_Team`) 
+    $query = "INSERT INTO `Student_Info` (`First_Name`, `Last_Name`, `Grade`, `PC_Class`, `Student_Number`, `DOB`, `House_Team`) 
       VALUES ('$StuFirstName', '$StuLastName', '$StuYear', '$StuPC', '$StuNumber', '$StuDOB', '$StuHouse');";
-      if (mysqli_query($con, $query)) {
-        echo "New student added successfully";
-      } else {
-        echo "Error: " . $query . "<br>" . mysqli_error($con);
-      }
+    if (mysqli_query($con, $query)) {
+      echo "New student added successfully";
+    } else {
+      echo "Error: " . $query . "<br>" . mysqli_error($con);
     }
+  }
 
 
 
@@ -170,39 +170,38 @@
         class="u-carousel u-carousel-duration-250 u-carousel-fade u-form u-progress-text-hidden-sm u-progress-text-hidden-xs u-form-1"
         data-interval="0" data-u-ride="false">
         <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"
-          class="u-clearfix u-form-spacing-10 custom-form u-inner-form" source="" name="form"
-          style="padding: 10px;">
+          class="u-clearfix u-form-spacing-10 custom-form u-inner-form" source="" name="form" >
           <div class="u-carousel-inner" role="listbox">
             <div class="u-carousel-item u-form-step u-slide u-active">
-              <div class="u-form-group u-form-name u-form-partition-factor-2">
+              <div class="u-form-group u-form-name u-form-partition-factor-2" style="text-align:left; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;">
                 <label for="name-883e" class="u-label">First Name</label>
                 <input type="text" placeholder="Enter First Name of the Student" value="<?php echo $StuFirstName; ?>"
                   name="StuFirstName" class="u-input u-input-rectangle">
               </div>
-              <div class="u-form-group u-form-partition-factor-2 u-form-group-2">
+              <div class="u-form-group u-form-partition-factor-2 u-form-group-2" style="text-align:left; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;">
                 <label for="text-acc6" class="u-label">Last Name</label>
                 <input type="text" placeholder="Enter Last name of the Student" value="<?php echo $StuLastName; ?>"
                   name="StuLastName" class="u-input u-input-rectangle">
               </div>
-              <div class="u-form-email u-form-group">
+              <div class="u-form-email u-form-group" style="text-align:left; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;" >
                 <label for="email-883e" class="u-label">Email</label>
                 <input type="email" placeholder="Student's Email" value="<?php echo $StuEmail; ?>" name="StuEmail"
                   class="u-input u-input-rectangle" required="">
               </div>
-              <div class="u-form-date u-form-group u-form-partition-factor-3 u-form-group-4">
+              <div class="u-form-date u-form-group u-form-partition-factor-3 u-form-group-4" style="text-align:left; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;">
                 <label for="date-a19e" class="u-label">Date of Birth</label>
                 <input type="text" placeholder="MM/DD/YYYY" name="StuDOB" value="<?php echo $StuDOB; ?>"
                   class="u-input u-input-rectangle" required="" data-date-format="yyyy/mm/dd">
               </div>
               <div
-                class="u-form-group u-form-number u-form-number-layout-number u-form-partition-factor-3 u-form-group-5">
+                class="u-form-group u-form-number u-form-number-layout-number u-form-partition-factor-3 u-form-group-5" style="text-align:left; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;">
                 <label for="number-0b03" class="u-label">Year Group</label>
                 <div class="u-input-row" data-value="0">
                   <input value="0" min="7" max="12" step="1" type="number" value="<?php echo $StuYear; ?>"
                     placeholder="" name="StuYear" class="u-input u-input-rectangle u-text-black">
                 </div>
               </div>
-              <div class="u-form-group u-form-partition-factor-3 u-form-select u-form-group-6">
+              <div class="u-form-group u-form-partition-factor-3 u-form-select u-form-group-6" style="text-align:left; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;">
                 <label for="select-15af" class="u-label">House Group</label>
                 <div class="u-form-select-wrapper">
                   <select name="StuHouse" class="u-input u-input-rectangle">
@@ -220,23 +219,129 @@
                   </svg>
                 </div>
               </div>
-              <div class="u-form-group u-form-partition-factor-2 u-form-group-7">
+              <div class="u-form-group u-form-partition-factor-2 u-form-group-7" style="text-align:left; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;">
                 <label for="text-d46f" class="u-label">PC Class</label>
                 <input type="text" placeholder="" value="<?php echo $StuPC; ?>" name="StuPC"
                   class="u-input u-input-rectangle">
               </div>
-              <div class="u-form-group u-form-partition-factor-2 u-form-group-8">
+              <div class="u-form-group u-form-partition-factor-2 u-form-group-8" style="text-align:left; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;">
                 <label for="text-9a7c" class="u-label">Student Number</label>
                 <input type="text" placeholder="" value="<?php echo $StuNumber; ?>" name="StuNumber"
                   class="u-input u-input-rectangle">
               </div>
+              </div>
+              <div class="u-carousel-item u-form-step u-slide">
+              <div class="u-form-group u-form-partition-factor-2 u-form-group-9" style="text-align:left; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;">
+                <label for="text-5181" class="u-label">Parent 1 First Name</label>
+                <input type="text" placeholder="" id="text-5181" name="text" class="u-input u-input-rectangle">
+              </div>
+              <div class="u-form-group u-form-partition-factor-2 u-form-group-10" style="text-align:left; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;">
+                <label for="text-c3c2" class="u-label">Parent 2 First Name</label>
+                <input type="text" placeholder="" id="text-c3c2" name="text-3" class="u-input u-input-rectangle">
+              </div>
+              <div class="u-form-group u-form-partition-factor-2 u-form-group-11" style="text-align:left; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;">
+                <label for="text-b533" class="u-label">Parent 1 Last Name</label>
+                <input type="text" placeholder="" id="text-b533" name="text-4" class="u-input u-input-rectangle">
+              </div>
+              <div class="u-form-group u-form-partition-factor-2 u-form-group-12" style="text-align:left; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;">
+                <label for="text-40f6" class="u-label">Parent 2 Last Name</label>
+                <input type="text" placeholder="" id="text-40f6" name="text-5" class="u-input u-input-rectangle">
+              </div>
+              <div class="u-form-group u-form-partition-factor-2 u-form-group-13" style="text-align:left; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;">
+                <label for="text-9af3" class="u-label">Home Address</label>
+                <input type="text" placeholder="" id="text-9af3" name="text-1" class="u-input u-input-rectangle">
+              </div>
+              <div class="u-form-group u-form-partition-factor-2 u-form-group-14" style="text-align:left; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;">
+                <label for="text-6c73" class="u-label">Home Address (Secondary)</label>
+                <input type="text" placeholder="" id="text-6c73" name="text-2" class="u-input u-input-rectangle">
+              </div>
+              <div class="u-form-group u-form-partition-factor-2 u-form-phone u-form-group-15" style="text-align:left; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;">
+                <label for="phone-c448" class="u-label">Parent 1 Mobile Phone</label>
+                <input type="tel"
+                  pattern="\+?\d{0,3}[\s\(\-]?([0-9]{2,3})[\s\)\-]?([\s\-]?)([0-9]{3})[\s\-]?([0-9]{2})[\s\-]?([0-9]{2})"
+                  placeholder="Enter your phone (e.g. +14155552675)" id="phone-c448" name="phone"
+                  class="u-input u-input-rectangle">
+              </div>
+              <div class="u-form-group u-form-partition-factor-2 u-form-phone u-form-group-16" style="text-align:left; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;">
+                <label for="phone-dd1c" class="u-label">Parent 2 Mobile Phone</label>
+                <input type="tel"
+                  pattern="\+?\d{0,3}[\s\(\-]?([0-9]{2,3})[\s\)\-]?([\s\-]?)([0-9]{3})[\s\-]?([0-9]{2})[\s\-]?([0-9]{2})"
+                  placeholder="Enter your phone (e.g. +14155552675)" id="phone-dd1c" name="phone-1"
+                  class="u-input u-input-rectangle">
+              </div>
+              <div class="u-form-group u-form-partition-factor-2 u-form-phone u-form-group-17" style="text-align:left; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;">
+                <label for="phone-11a2" class="u-label">Parent 1 Work Phone</label>
+                <input type="tel"
+                  pattern="\+?\d{0,3}[\s\(\-]?([0-9]{2,3})[\s\)\-]?([\s\-]?)([0-9]{3})[\s\-]?([0-9]{2})[\s\-]?([0-9]{2})"
+                  placeholder="Enter your phone (e.g. +14155552675)" id="phone-11a2" name="phone-2"
+                  class="u-input u-input-rectangle">
+              </div>
+              <div class="u-form-group u-form-partition-factor-2 u-form-phone u-form-group-18" style="text-align:left; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;">
+                <label for="phone-1927" class="u-label">Parent 2 Work Phone</label>
+                <input type="tel"
+                  pattern="\+?\d{0,3}[\s\(\-]?([0-9]{2,3})[\s\)\-]?([\s\-]?)([0-9]{3})[\s\-]?([0-9]{2})[\s\-]?([0-9]{2})"
+                  placeholder="Enter your phone (e.g. +14155552675)" id="phone-1927" name="phone-3"
+                  class="u-input u-input-rectangle">
+              </div>
+              <div class="u-form-group u-form-partition-factor-2 u-form-phone u-form-group-19" style="text-align:left; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;">
+                <label for="phone-1531" class="u-label">Home Phone</label>
+                <input type="tel"
+                  pattern="\+?\d{0,3}[\s\(\-]?([0-9]{2,3})[\s\)\-]?([\s\-]?)([0-9]{3})[\s\-]?([0-9]{2})[\s\-]?([0-9]{2})"
+                  placeholder="Enter your phone (e.g. +14155552675)" id="phone-1531" name="phone-4"
+                  class="u-input u-input-rectangle">
+              </div>
+              <div class="u-form-group u-form-partition-factor-2 u-form-phone u-form-group-20" style="text-align:left; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;">
+                <label for="phone-9df4" class="u-label">Home Phone (Secondary)</label>
+                <input type="tel"
+                  pattern="\+?\d{0,3}[\s\(\-]?([0-9]{2,3})[\s\)\-]?([\s\-]?)([0-9]{3})[\s\-]?([0-9]{2})[\s\-]?([0-9]{2})"
+                  placeholder="Enter your phone (e.g. +14155552675)" id="phone-9df4" name="phone-5"
+                  class="u-input u-input-rectangle">
+              </div>
+            </div>
+            <div class="u-carousel-item u-form-step u-slide">
+              <div class="u-form-group u-form-group-21" style="text-align:left; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;">
+                <label for="text-a614" class="u-label">Subject 1</label>
+                <input type="text" placeholder="" id="text-a614" name="text" class="u-input u-input-rectangle">
+              </div>
+              <div class="u-form-group u-form-group-22" style="text-align:left; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;">
+                <label for="text-6d19" class="u-label">Subject 2</label>
+                <input type="text" placeholder="" id="text-6d19" name="text-1" class="u-input u-input-rectangle">
+              </div>
+              <div class="u-form-group u-form-group-23" style="text-align:left; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;">
+                <label for="text-9824" class="u-label">Subject 3</label>
+                <input type="text" placeholder="" id="text-9824" name="text-2" class="u-input u-input-rectangle">
+              </div>
+              <div class="u-form-group u-form-group-24" style="text-align:left; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;">
+                <label for="text-bd56" class="u-label">Subject 4</label>
+                <input type="text" placeholder="" id="text-bd56" name="text-3" class="u-input u-input-rectangle">
+              </div>
+              <div class="u-form-group u-form-group-25" style="text-align:left; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;">
+                <label for="text-60ac" class="u-label">Subject 5</label>
+                <input type="text" placeholder="" id="text-60ac" name="text-4" class="u-input u-input-rectangle">
+              </div>
+              <div class="u-form-group u-form-group-26" style="text-align:left; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;">
+                <label for="text-0a0b" class="u-label">Subject 6</label>
+                <input type="text" placeholder="" id="text-0a0b" name="text-5" class="u-input u-input-rectangle">
+              </div>
+              <div class="u-form-group u-form-partition-factor-2 u-form-group-27" style="text-align:left; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;">
+                <label for="text-225e" class="u-label">Subject 7</label>
+                <input type="text" placeholder="" id="text-225e" name="text-6" class="u-input u-input-rectangle">
+              </div>
+              <div class="u-form-group u-form-partition-factor-2 u-form-group-28" style="text-align:left; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;">
+                <label for="text-368e" class="u-label">Subject 8</label>
+                <input type="text" placeholder="" id="text-368e" name="text-7" class="u-input u-input-rectangle">
+              </div>
+              <div class="u-form-group u-form-group-21" style="text-align:left; padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px;">
+                <input type="submit" value="Submit" name="Submit" class="u-btn u-button-style">
+            </div>
+              
             </div>
 
           </div>
           <div class="u-align-left u-form-group u-form-submit">
             <a href="#" class="u-btn u-btn-step u-btn-step-prev u-button-style u-hidden">Back</a>
             <a href="#" class="u-btn u-btn-step u-btn-step-next u-button-style">Next</a>
-            <input type="submit" value="Submit" name="Submit" class="">
+            
 
             <input type="hidden" value="" name="recaptchaResponse">
             <input type="hidden" value="" name="recaptchaResponse">
